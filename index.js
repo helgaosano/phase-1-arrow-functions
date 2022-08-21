10cee13b91ace9dcae28afa26ea1348ebbd36a59
tania <taniaosano15@gmail.com>
2022-08-21 20:51:22 +0300

    Completed arrow functions lab assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+/*const divide = (num1, num2) => { 
+    let result = num1 / num2;
+    return result;
+}
+divide(2000, 100);
+
+const square = (10) => 
+*/
+const divide = (num1, num2) => {
+    return 2000 / 100;
+}
+divide()
+
+const square = (x) => x * x;
+square(2);
+
+const add = (a, b) => a + b;
+add(3, 4)
